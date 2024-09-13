0e6767f2b417925c6fa2ecc8c2803bf98b21220d
= <=>
2024-09-13 13:48:27 +0000

    Hypermedia pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -4,7 +4,7 @@ Implement a Class that Processes csv dataset
 '''
 import csv
 import math
-from typing import List
+from typing import List, Dict
 index_range = __import__('0-simple_helper_function').index_range
 
 
@@ -31,8 +31,8 @@ class Server:
         ''' Returns a page of results given the number and size '''
         self.dataset()
         assert (isinstance(page, int) and isinstance(page_size, int))
+        print(f'page:{page} => page_size:{page_size}')
         assert page > 0 and page_size > 0
-
 
         idxs = index_range(page, page_size)
 
@@ -40,3 +40,24 @@ class Server:
             return []
 
         return self.__dataset[idxs[0]:idxs[1]]
+
+    def get_hyper(self, page: int = 1, page_size: int = 10) -> Dict:
+        '''
+        Returns some statistics about the dataset
+        '''
+        dataset = self.dataset()
+        get_page = self.get_page
+        data = get_page(page, page_size)
+        p_size = len(data)  # length of the returned data
+        next_page = page + 1 if len(get_page(page + 1, page_size)) else None
+        prev_page = page - 1 if page != 1 else None
+        total_pages = math.ceil(len(dataset) / page_size)
+
+        return {
+            'page_size': p_size,
+            'page': page,
+            'data': data,
+            'next_page': next_page,
+            'prev_page': prev_page,
+            'total_pages': total_pages
+        }
