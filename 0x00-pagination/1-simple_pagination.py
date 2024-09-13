1c9af807a92076c5e07b0bcd2e19962210aaa08a
= <=>
2024-09-13 09:36:58 +0000

    Simple Pagination

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -28,6 +28,7 @@ class Server:
         return self.__dataset
 
     def get_page(self, page: int = 1, page_size: int = 10) -> List[List]:
+        ''' Returns a page of results given the number and size '''
         self.dataset()
         assert (isinstance(page, int) and isinstance(page_size, int))
         assert page > 0 and page_size > 0
