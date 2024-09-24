433c64275ecebc3ba8789df56d634f61d1c99ffd
= <=>
2024-09-24 20:11:28 +0000

    Basic Flask app

---
+++ 0x02-i18n/0-app.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python3
+'''
+Basic Flask App
+'''
+from flask import Flask, render_template
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def index_page():
+    return render_template('0-index.html')
+
+
+if __name__ == '__main__':
+    app.run(port='5000', host='127.0.0.1')
