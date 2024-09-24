afec1d6a1455f5cf13db4bd4034a7a99387e3c16
= <=>
2024-09-24 21:51:23 +0000

    Basic Babel setup

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+''' Basic Bable setup '''
+from flask import Flask, render_template
+from flask_babel import Babel
+
+
+class Config:
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/')
+def index():
+    """ index view """
+    return render_template("1-index.html")
+
+
+if __name__ == '__main__':
+    app.run(host='127.0.0.1', port='5000')
