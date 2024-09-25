83ebb252cbee3eaa20caa22ecac5677af942ef70
= <=>
2024-09-25 21:25:45 +0000

    Parameterised templates

---
+++ 0x02-i18n/3-app.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+""" PAramiterize templates """
+from flask import Flask, render_template, request
+from flask_babel import Babel, gettext as _
+Config = __import__("1-app").Config
+
+
+app = Flask(__name__)
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@babel.localeselector
+def get_locale():
+    return request.accept_languages.best_match(app.config["LANGUAGES"])
+
+
+@app.route("/")
+def index():
+    """ index view """
+    home_title = _("Welcome to Holberton")
+    home_header = _("Hello world!")
+    return render_template(
+            "3-index.html",
+            home_title=home_title, home_header=home_header)
+
+
+if __name__ == "__main__":
+    app.run(port='5000', debug=True)
