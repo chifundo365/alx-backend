a939444e772d9d96493b733bcb6299d12a3d50c3
= <=>
2024-09-27 20:50:40 +0000

    Parametrize templates

--- 0x02-i18n/3-app.py
+++ 0x02-i18n/3-app.py
@@ -16,7 +16,7 @@ def get_locale():
     return request.accept_languages.best_match(app.config["LANGUAGES"])
 
 
-@app.route("/")
+@app.route("/", strict_slashes=False)
 def index():
     """ index view """
     home_title = _("Welcome to Holberton")
